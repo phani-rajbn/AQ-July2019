87f7b5c7882920bc42700801d73398a0244727d3
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2019-07-17 10:16:31 +0530

    Add files via upload

---
+++ DotnetTraining/SampleConApp/MainProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ITicketManager mgr = new TicketManager();
+                //mgr.AddTicket(Place.Bangalore, Place.Hyderabad, DateTime.Now.AddDays(-25), 960);
+                //Console.WriteLine("Ticket is successfully added");
+                var amount = mgr.CancelTicket(2);
+                Console.WriteLine("The cancelled amount is " + amount);
+                var data = mgr.FindTickets(Place.Hyderabad, Place.Bangalore);
+                foreach (DataRow row in data.Rows)
+                {
+                    Console.WriteLine($"Date:{row[3]}\nSource:{row[1]}\nDestination:{row[2]}\nPrice:{row[4]:C}");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ DotnetTraining/SampleConApp/TicketManager.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+    enum Place { Bangalore, Chennai, Hyderabad, Hubli, Mangalore };
+    interface ITicketManager
+    {
+        void AddTicket(Place destination, Place source, DateTime dateOfJourney, int amount);
+        void Modify(int TicketID, Place destination, Place source, DateTime dateOfJourney, int amount);
+        double CancelTicket(int ticketId);
+        DataTable FindTickets(Place source, Place destination);
+        DataTable FindTickets(DateTime dateOfJourney);
+    }
+    class TicketManager : ITicketManager
+    {
+        const string fileName = "Data.Bin";
+
+        DataTable table = null;
+        public TicketManager()
+        {
+            table = deSerialize();
+            if (table == null)
+            {
+                table = new DataTable("Tickets");
+                //create the table structure...
+                table.Columns.Add("TicketID", typeof(int));
+                table.Columns.Add("Source", typeof(string));
+                table.Columns.Add("Destination", typeof(string));
+                table.Columns.Add("Date", typeof(DateTime));
+                table.Columns.Add("Amount", typeof(int));
+                table.PrimaryKey = new DataColumn[] { table.Columns[0] };
+                table.AcceptChanges();
+            }
+        }
+        public void AddTicket(Place destination, Place source, DateTime dateOfJourney, int amount)
+        {     
+            DataRow row = table.NewRow();//Creates a blank row with the schema of the table cols...
+            row[0] = row.Table.Rows.Count + 1;
+            row[1] = source;
+            row[2] = destination;
+            row[3] = dateOfJourney;
+            row[4] = amount;
+            table.Rows.Add(row);//Add the row to the table's Rows Collection.
+            table.AcceptChanges();
+            serializeData(table);
+        }
+
+        private DataTable deSerialize()
+        {
+            if (File.Exists(fileName))
+            {
+                using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+                {
+                    BinaryFormatter fm = new BinaryFormatter();
+                    var table = fm.Deserialize(fs) as DataTable;
+                    return table;
+                }
+            }
+            return null;
+        }
+        private void serializeData(DataTable table)
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                BinaryFormatter fm = new BinaryFormatter();
+                fm.Serialize(fs, table);
+            }
+        }
+
+        public double CancelTicket(int ticketId)
+        {
+            table = deSerialize();
+            foreach(DataRow row in table.Rows)
+            {
+                if(row[0].ToString() == ticketId.ToString())
+                {
+                    var amount = Convert.ToDouble(row[4]);
+                    amount -= (amount * 15 / 100);
+                    row.Delete();//Sets the state as delete.
+                    table.AcceptChanges();
+                    return amount;
+                }
+            }
+            serializeData(table);
+            return 0;
+        }
+
+        public DataTable FindTickets(Place source, Place destination)
+        {
+            if (table == null)
+                table = deSerialize();
+            var copy = table.Copy();
+            copy.Rows.Clear();//Clears all the rows....
+            foreach(DataRow row in table.Rows)
+            {
+                if((row[1].ToString() == source.ToString()) && (row[2].ToString() == destination.ToString()))
+                {
+                    DataRow newRow = copy.NewRow();
+                    for (int i = 0; i < row.ItemArray.Length; i++)
+                    {
+                        newRow[i] = row[i];
+                    }
+                    copy.Rows.Add(newRow);
+                    copy.AcceptChanges();
+                }
+            }
+            return copy;
+        }
+
+        public DataTable FindTickets(DateTime dateOfJourney)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modify(int TicketID, Place destination, Place source, DateTime dateOfJourney, int amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
