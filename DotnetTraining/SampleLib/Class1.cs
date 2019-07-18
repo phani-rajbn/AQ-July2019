4091af8d5ca9aee8c10e38ba330f91a2747b8bd8
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2019-07-18 12:56:36 +0530

    Add files via upload

---
+++ DotnetTraining/SampleLib/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleLib
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string Empname { get; set; }
+        public string EmpAddress { get; set; }
+        public double EmpSalary { get; set; }
+        public long PhoneNo { get; set; }
+        public override bool Equals(object obj)
+        {
+            if (obj is Employee)
+            {
+                var temp = obj as Employee;
+                return temp.EmpID == this.EmpID;
+            }
+            else return false;
+        }
+        //2 objects are said to be equal if their hashcodes are same and the equals method returns true....
+        public override int GetHashCode()
+        {
+            return EmpID.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"Name:{Empname}\tAddress:{EmpAddress}\tEmpSalary:{EmpSalary:C},\tEmpPhone:{PhoneNo}");
+        }
+
+
+    }
+}
