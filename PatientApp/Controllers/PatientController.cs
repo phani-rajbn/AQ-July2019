6c0de5467f86cf9e0cc003917f45940f89fd7c11
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2019-07-27 13:08:26 +0530

    Add files via upload

---
+++ PatientApp/Controllers/PatientController.cs
@@ -0,0 +1,88 @@
+﻿using PatientApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PatientApp.Controllers
+{
+    public class PatientController : Controller
+    {
+        // GET: Patient
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult NewPatient()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewPatient(PatientTable patient)
+        {
+            var context = new PatientDataContext();
+            context.PatientTables.InsertOnSubmit(patient);
+            context.SubmitChanges();
+            return RedirectToAction("Dashboard");
+        }
+
+        
+        public JsonResult FindDoctors(string id)
+        {
+            var context = new PatientDataContext();
+            var docs = from doc in context.DoctorTables
+                       where doc.Speciality == id
+                       select new
+                       {
+                           doc.DoctorID,
+                           doc.Doctor,
+                           doc.Fees
+                       };
+
+            return Json(docs.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Fees(string id)
+        {
+            var docId = int.Parse(id);
+            var context = new PatientDataContext();
+            var doc = context.DoctorTables.FirstOrDefault(d => d.DoctorID == docId);
+            return Json(doc.Fees, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult FindInfo()
+        {
+            return PartialView();
+        }
+        public JsonResult PId(string id)
+        {
+            var pId = int.Parse(id);
+            var context = new PatientDataContext();
+            var patient = context.PatientTables.FirstOrDefault(p => p.PatientID == pId);
+            if (patient == null)
+            {
+                pId = context.PatientTables.Max(p => p.PatientID) + 1;
+                var data = new
+                {
+                    PatientID = pId,
+                    PatientName = string.Empty,
+                    PatientContact = 0
+                };
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                var data = new
+                {
+                    patient.PatientID,
+                    patient.PatientName,
+                    patient.PatientContact
+                };
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+    }
+}
---
+++ PatientApp/Controllers/PatientInfoController.cs
@@ -0,0 +1,41 @@
+﻿using PatientApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PatientApp.Controllers
+{
+    public class PatientInfoController : ApiController
+    {
+        public List<PatientDetails> GetAllPatients(string id)
+        {
+            var pId = int.Parse(id);
+            var context = new PatientDataContext();
+            var list = from p in context.PatientTables
+                       where p.PatientID == pId
+                       orderby p.EntryId descending
+                       select new PatientDetails
+                       {
+                           PatientID = p.PatientID,
+                          Name = p.PatientName,
+                          Disease =  p.Disease,
+                          Doctor = p.DoctorTable.Doctor,
+                          Amount= p.Amount
+                       };
+            return list.ToList();
+        }
+    }
+
+    public class PatientDetails
+    {
+        public int PatientID { get; set; }
+        public string Name { get; set; }
+        public long Contact { get; set; }
+        public string Disease { get; set; }
+        public string Doctor { get; set; }
+        public int Amount { get; set; }
+    }
+}
